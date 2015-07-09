9caf5fd08461c7ebf9fa0b0932b3060d04265749
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-07-09 22:32:38 +0300

    + scAlertProvider with configuration

--- scAlert.js
+++ scAlert.js
@@ -4,56 +4,99 @@
 
     angular
         .module('sc-alert', [])
-        .constant('config', {
-            parent: 'body',
-            html: false,
-            dismissible: true,
-            timeout: 5000,
-            // Element attach method:angular 'append' or 'prepend'
-            method: 'prepend'
+        .provider('scAlert', function () {
+            var config = {
+                parent: 'body',
+                html: false,
+                dismissible: true,
+                timeout: 5000,
+                // Element attach method:angular 'append' or 'prepend'
+                method: 'prepend'
+            };
+            var titles = {
+                success: "Success",
+                danger: "Danger",
+                info: "Info",
+                warning: "Warning"
+            };
+            this.setConfig = function (new_config) {
+                config = angular.extend(config, new_config);
+            };
+            this.setTitles = function (new_titles) {
+                titles = angular.extend(titles, new_titles);
+            };
+            this.$get = function() {
+                return {
+                    config: config,
+                    titles: titles
+                };
+            };
         })
         .run(['$templateCache', function ($templateCache) {
             if ($templateCache.get('templates/scAlert/alert.html') === undefined) {
                 $templateCache.put("templates/scAlert/alert.html",
                     '<div ng-if="!destroy" ng-class="classes">' +
-                        '<div ng-if="config.dismissible" type="button" class="close" aria-label="Close">' +
+                        '<div ng-if="params.dismissible" type="button" class="close" aria-label="Close">' +
                             '<span aria-hidden="true" ng-click="close()">&times;</span>' +
                         '</div>' +
                         '<strong ng-bind="title"></strong> ' +
-                        '<span ng-if="config.html" ng-bind-html="content"></span>' +
-                        '<span ng-if="!config.html" ng-bind="content"></span>' +
+                        '<span ng-if="params.html" ng-bind-html="text"></span>' +
+                        '<span ng-if="!params.html" ng-bind="text"></span>' +
                     '</div>'
                 );
             }
         }])
-        .service('scAlert', function($templateCache, $compile, $document, $timeout, $sce, config) {
-            var self = this;
-            self.template = $templateCache.get('templates/scAlert/alert.html');
+        .directive('scAlert', function ($sce, $timeout, scAlertService, scAlert) {
+            return {
+                restrict: 'E',
+                templateUrl: "templates/scAlert/alert.html",
+                scope: { config: "@", type: "@", content: "=" },
+                link: function (scope, element, attributes) {
+                    scope.destroy = false;
+                    scope.title = scope.title || scAlert.titles[scope.type];
+                    scope.params = scAlertService.mergeConfig(scope.$eval(scope.config));
+                    scope.text = scope.params.html ? $sce.trustAsHtml(scope.content) : scope.content;
+                    scope.classes = angular.extend(scAlertService.alertClasses(scope.type), scope.params.classes);
+                    scope.close = function () {
+                        scope.$destroy();
+                        element.remove();
+                    };
+                    if (scope.params.timeout) {
+                        $timeout(scope.close, scope.params.timeout);
+                    }
 
+                }
+            };
+        })
+        .service('scAlertService', function($templateCache, $compile, $document, scAlert) {
+            var self = this;
+            self.template = '<sc-alert type="{{type}}" config="{{config}}" content="content" title="title"></sc-alert>';
             self.alert = function(type, content, title, user_config) {
                 var scope = this.$new(true);
-                scope.config = mergeConfig(user_config);
-                scope.classes = angular.extend(alertClasses(type), scope.config.classes);
-                scope.title = title || type.charAt(0).toUpperCase() + type.slice(1);
-                scope.content = scope.config.html ? $sce.trustAsHtml(content) : content;
-                scope.close = function () {
-                    scope.destroy = true;
-                };
-                if (scope.config.timeout) {
-                    $timeout(scope.close, scope.config.timeout);
-                }
+                scope.type = type;
+                scope.title = title;
+                scope.content = content;
+                scope.config = self.mergeConfig(user_config);
                 scope.alert = $compile(self.template)(scope);
                 getParent(scope.config.parent)[scope.config.method](scope.alert);
             };
-
             self.success = function (content, caption, user_config) { self.alert('success', content, caption, user_config) };
             self.danger = function (content, caption, user_config) { self.alert('danger', content, caption, user_config) };
             self.info = function (content, caption, user_config) { self.alert('info', content, caption, user_config) };
             self.warning = function (content, caption, user_config) { self.alert('warning', content, caption, user_config) };
+            self.mergeConfig = function(new_config) {
+                return angular.extend({}, scAlert.config, new_config);
+            };
+            self.alertClasses = function (type) {
+                return {
+                    'alert': true,
+                    'alert-success': type === "success",
+                    'alert-danger': type === "danger",
+                    'alert-info': type === "info",
+                    'alert-warning': type === "warning"
+                };
+            };
 
-            function mergeConfig (new_config) {
-                return angular.extend({}, config, new_config);
-            }
             function getParent (selector) {
                 switch(selector[0]) {
                     case ".":
@@ -63,15 +106,6 @@
                     default:
                         return angular.element(document.getElementsByTagName(selector)[0]);
                 }
-            }
-            function alertClasses (type) {
-                return {
-                    'alert': true,
-                    'alert-success': type === "success",
-                    'alert-danger': type === "danger",
-                    'alert-info': type === "info",
-                    'alert-warning': type === "warning"
-                };
             }
         });
 
